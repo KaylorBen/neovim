4b48b084a2a53b7ae0972450e18d9cbff43116db
Benjamin Kaylor <blkaylor22@gmail.com>
2024-08-21 11:34:14 -0600

    testing this

--- lua/nixy/vimtex.lua
+++ lua/nixy/vimtex.lua
@@ -1,7 +1,2 @@
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-  pattern = "*.tex",
-  callback = function ()
-    vim.cmd("packadd vimtex")
-    vim.g.vimtex_view_method = 'zathura'
-  end,
-})
+vim.cmd("packadd vimtex")
+vim.g.vimtex_view_method = 'zathura'
