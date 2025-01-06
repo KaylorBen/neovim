16423af8bbed2104bf554044ad02d2d007ad8c70
Benjamin Kaylor <blkaylor22@gmail.com>
2025-01-06 12:44:32 -0700

    lausnip htlm tags in js/ts

--- lua/nixy/cmp.lua
+++ lua/nixy/cmp.lua
@@ -3,9 +3,8 @@ vim.api.nvim_set_hl(0, "CmpItemKindEmoji", { fg = "#FDE030" })
 
 local cmp = require("cmp")
 local luasnip = require("luasnip")
-
-luasnip.filetype_extend("javascriptreact", { "html" })
-
+luasnip.filetype_extend("javascript", { "html" })
+luasnip.filetype_extend("typescript", { "html" })
 require("luasnip.loaders.from_vscode").lazy_load()
 
 cmp.setup({
